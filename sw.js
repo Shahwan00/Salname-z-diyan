64e087f407732c299279a0997e0c16913e2b3be1
ezidicode.com <shahwanshahwan71@gmail.com>
2026-08-23 12:39:29 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="ezidi-universal-v4";const A=["./","./index.html","./manifest.json","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
